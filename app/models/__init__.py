b0a02657dc2c24944a1291165dab1f0a7d5d2e20
EldarJc <jusiceldar87@gmail.com>
2025-11-17 22:13:51 +0100

    Add TYPE_CHECKING alias and utc_now helper

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,6 +1,21 @@
+from datetime import datetime, timezone
+from typing import TYPE_CHECKING
+
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
 db_migrate = Migrate()
+
+
+if TYPE_CHECKING:
+    from flask_sqlalchemy.model import Model
+
+    model = Model
+else:
+    model = db.Model
+
+
+def utc_now() -> datetime:
+    return datetime.now(timezone.utc)
