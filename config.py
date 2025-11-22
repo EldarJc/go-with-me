32c52ef8fe131fde3ab610f2688db822d0372476
EldarJc <jusiceldar87@gmail.com>
2025-11-22 02:23:16 +0100

    Add instance_dir

--- config.py
+++ config.py
@@ -4,10 +4,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+base_dir = os.path.abspath(os.path.dirname(__file__))
+instance_dir = os.path.join(base_dir, "instance")
+
 
 class BaseConfig:
-    SECRET_KEY = os.environ["SECRET_KEY"]
-    SQLALCHEMY_DATABASE_URI = os.environ["DB_PATH"]
+    SECRET_KEY = os.environ.get("SECRET_KEY", "app_secret_key")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DB_ENGINE", "sqlite:///") + os.path.join(
+        instance_dir, "test_data.db"
+    )
 
 
 class DevConfig(BaseConfig):
