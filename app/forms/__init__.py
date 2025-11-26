1a707e3e51f81fefafacb99d0a1807f54347d289
EldarJc <jusiceldar87@gmail.com>
2025-11-26 23:32:07 +0100

    Add forms package initializer

---
+++ app/forms/__init__.py
@@ -0,0 +1,6 @@
+REQUIRED_MSG = "This field cannot be empty."
+
+from .event_form import CreateEventForm
+from .group_form import CreateGroupForm
+from .login_form import LoginForm
+from .signup_form import SignUpForm
