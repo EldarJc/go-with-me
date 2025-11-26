202871b27ce167f56658bc1acabbec361e9b98df
EldarJc <jusiceldar87@gmail.com>
2025-11-26 23:27:15 +0100

    Add user login form

---
+++ app/forms/login_form.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import BooleanField, PasswordField, StringField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class LoginForm(FlaskForm):
+    email_username = StringField("Username or Email", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    remember_me = BooleanField("Remember Me", default=False)
+    submit = SubmitField("Log in")
